4e6954d866f56311d6675b8cc55f0feb43265f5c
GillisC <timcarlsson0203@gmail.com>
2021-11-17 14:31:28 +0100

    - All methods are tested

--- src/Car.java
+++ src/Car.java
@@ -6,8 +6,8 @@ abstract class Car {
     protected double currentSpeed; // The current speed of the car
     protected Color color; // Color of the car
     protected String modelName; // The car model name
-    protected int x;
-    protected int y;
+    protected double x;
+    protected double y;
     protected int dir; // The Car's direction in Degrees
     protected boolean turboOn;
     protected boolean engineOn;
--- test/CarTest.java
+++ test/CarTest.java
@@ -4,13 +4,21 @@ import java.awt.*;
 import java.awt.image.ConvolveOp;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CarTest {
 
     public Volvo240 volvo = new Volvo240();
     public Saab95 saab = new Saab95();
 
-
+    @Test
+    public void volvo_doors_four(){
+        assertEquals(4, volvo.getNrDoors());
+    }
+    @Test
+    public void volvo_engine_100(){
+        assertEquals(100, volvo.getEnginePower(), 0.01);
+    }
     @Test
     public void car_should_turn_45_degrees() {
         Volvo240 volvo = new Volvo240();
@@ -30,22 +38,38 @@ public class CarTest {
     }
     @Test
     public void should_not_be_able_to_incrementSpeed_with_engine_off(){
-        Volvo240 volvo = new Volvo240();
+        Volvo240 volvo = new Volvo240(); // A car should not be able to accelerate without the engine being on
         volvo.stopEngine();
-        volvo.incrementSpeed(10);
+        volvo.incrementSpeed(1);
         assertEquals(0, volvo.getCurrentSpeed(), 0.01);
     }
-//    @Test
-//    public void next_position(){
-//        Volvo240 volvo = new Volvo240();
-//        volvo.x = 0;
-//        volvo.y = 0;
-//        volvo.startEngine();
-//        volvo.gas(0.9);
-//        volvo.dir = 90;
-//        volvo.move();
-//        assertEquals(1, volvo.y);
-//    }
+    @Test
+    public void move_not_should_move_if_engineOff(){
+        Volvo240 volvo = new Volvo240();
+        volvo.dir = 45;
+        volvo.stopEngine();
+        volvo.move();
+        assertEquals(0, volvo.x, 0.01);
+    }
+    @Test
+    public void should_move_if_engineOn(){
+        Volvo240 volvo = new Volvo240();
+        volvo.dir = 90;
+        volvo.y = 0;
+        volvo.startEngine();
+        volvo.gas(1);
+        volvo.move();
+        assertTrue(volvo.y > 0);
+    }
+    @Test
+    public void brake_should_lower_currentSpeed(){
+        Volvo240 volvo = new Volvo240();
+        volvo.startEngine();
+        volvo.gas(1);
+        double v0 = volvo.getCurrentSpeed();
+        volvo.brake(0.5);
+        assertTrue(volvo.getCurrentSpeed() < v0);
+    }
     @Test
     public void turn_right_should_45(){
         Volvo240 volvo = new Volvo240();
